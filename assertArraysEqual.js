076c865a7d24d07de61eefc8a3e2623bc4eedafd
Marcello Kuenzler <marcello.kuenzler@gmail.com>
2020-09-15 22:48:58 +0000

    Fixed array1/2 to actual/expected

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -4,9 +4,9 @@ const eqArrays = function(array1, array2) {
   return true;
 }
 
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1,array2)) console.log(`✅✅✅Assertion Passed: ${array1} === ${array2}`);
-  else console.log(`🛑🛑🛑Assertion Failed: ${array1} !== ${array2}`);
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual,expected)) console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  else console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
 };
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]) // => true
