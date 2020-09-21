48650752511f5d966c43a82560b494359b579801
Marcello Kuenzler <marcello.kuenzler@gmail.com>
2020-09-21 10:01:29 -0600

    Refactored Middle and Tail Tests for Mocha and Chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,12 +1,16 @@
-const tail = require('../tail');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+const tail   = require('../tail');
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
+describe("#tail", () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
+  });
 
-const empty = [];
-const one = [1];
-assertEqual(tail(empty).length, 0);
-assertEqual(tail(one).length, 0);
+  it("returns [] for []", () => {
+    assert.deepEqual(tail([]), []); 
+  });
+
+  it("returns [] for [1]", () => {
+    assert.deepEqual(tail([1]), []); 
+  });
+});
