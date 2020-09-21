79ccded516d2e43f98349a920ed35c4a90cddb2e
Marcello Kuenzler <marcello.kuenzler@gmail.com>
2020-09-21 09:48:08 -0600

    Refactor Middle for Modules and Testing

--- middle.js
+++ middle.js
@@ -1,14 +1,3 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) return false;
-  for (let i = 0; i < array1.length; i++)  if (array1[i] !== array2[i]) return false;
-  return true;
-}
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1,array2)) console.log(`✅✅✅Assertion Passed: ${array1} === ${array2}`);
-  else console.log(`🛑🛑🛑Assertion Failed: ${array1} !== ${array2}`);
-};
-
 const middle = function(array) {
   if (array.length < 3) return [];
   let mid = [];
@@ -20,9 +9,4 @@ const middle = function(array) {
   return mid;
 }
 
-assertArraysEqual(middle([1]),[]);
-assertArraysEqual(middle([1, 2]),[]);
-assertArraysEqual(middle([1, 2, 3]),[2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]),[3]);
-assertArraysEqual(middle([1, 2, 3, 4]),[2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3, 4]);
+module.exports = middle;
