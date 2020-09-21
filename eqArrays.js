4f16775ba3267016311c14e03b0f5127a965a15e
Marcello Kuenzler <marcello.kuenzler@gmail.com>
2020-09-21 09:40:14 -0600

    Refactored eqArrays for Modules and Testing

--- eqArrays.js
+++ eqArrays.js
@@ -1,7 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  else console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-};
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) return false;
@@ -14,9 +11,4 @@ const eqArrays = function(array1, array2) {
   return true;
 }
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should PASS
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should PASS
-assertEqual(eqArrays(["1", ["2", "3"], "4"], ["1", ["2", "3"], "4"]), true); // => should PASS
-assertEqual(eqArrays(["1", ["2", [[["3"]]]], "4"], ["1", ["2", [[["3"]]]], "4"]), true); // => should PASS
+module.exports = eqArrays;
